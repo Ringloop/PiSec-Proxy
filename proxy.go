9f8caf4c5f9076ec2e43b59d1e426873e0f9b50b
Dario Balinzo <dariobalinzo@gmail.com>
2022-03-29 21:47:53 +0200

    blocking both http and https

--- proxy.go
+++ proxy.go
@@ -1,30 +1,53 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 	"net/http"
-	"time"
+	"strings"
 
 	"github.com/elazarl/goproxy"
 )
 
 func main() {
 	proxy := goproxy.NewProxyHttpServer()
-	proxy.OnRequest(IsMalware()).DoFunc(
-		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-			if h, _, _ := time.Now().Clock(); h >= 8 && h <= 24 {
-				return r, goproxy.NewResponse(r,
-					goproxy.ContentTypeText, http.StatusForbidden,
-					"Blocked By PiSec with <3 !")
-			}
-			return r, nil
-		})
+	proxy.OnRequest(IsMalwareRequestHttp()).DoFunc(GetPiSecPage)
+	proxy.OnRequest(IsMalwareRequestHttps()).HandleConnect(goproxy.AlwaysReject)
 	proxy.Verbose = true
+
 	log.Fatal(http.ListenAndServe(":8080", proxy))
 }
 
-func IsMalware() goproxy.ReqConditionFunc {
+func GetPiSecPage(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+	return r, goproxy.NewResponse(r,
+		goproxy.ContentTypeText, http.StatusForbidden,
+		"Blocked By PiSec with <3 !")
+}
+
+func GetPiSecPage2(r *http.Request, client net.Conn, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+	return r, goproxy.NewResponse(r,
+		goproxy.ContentTypeText, http.StatusForbidden,
+		"Blocked By PiSec with <3 !")
+}
+
+func IsMalwareRequestHttp() goproxy.ReqConditionFunc {
 	return func(req *http.Request, ctx *goproxy.ProxyCtx) bool {
-		return req.URL.Host == "news.ycombinator.com"
+		fmt.Println("checking...")
+		fmt.Println(req.Host)
+		return strings.Split(req.Host, ":")[0] == "news.ycombinator.com" //todo check bloom filter
 	}
+}
+
+func IsMalwareRequestHttps() goproxy.ReqConditionFunc {
+	return func(req *http.Request, ctx *goproxy.ProxyCtx) bool {
+		fmt.Println("checking...")
+		fmt.Println(req.Host)
+		return strings.Split(req.Host, ":")[0] == "news.ycombinator.com" //todo check bloom filter
+	}
+}
+
+var IsConnectToMalware goproxy.FuncHttpsHandler = func(host string, ctx *goproxy.ProxyCtx) (*goproxy.ConnectAction, string) {
+	fmt.Println("connecting...")
+	return goproxy.MitmConnect, host
 }
