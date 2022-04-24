f09eab22dff37ae64e2c9a07d6ecad5545c45def
Alessio Pascucci <feeder81@gmail.com>
2022-04-24 11:18:06 +0200

    Download filter and test request
    Download the bloom filter from the Brain
    Test the connection against the filter downloaded

--- proxy.go
+++ proxy.go
@@ -2,21 +2,44 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
+	"os"
 	"strings"
 
+	"github.com/bits-and-blooms/bloom/v3"
 	"github.com/elazarl/goproxy"
 )
 
+var filter *bloom.BloomFilter = bloom.NewWithEstimates(1000000, 0.01)
+var serverAddress string = os.Getenv("PISEC_BRAIN_ADDR")
+var indicatorsEndpoint string = "/api/v1/indicators"
+
 func main() {
+
+	//download the bloom filter from server
+	endpoint := serverAddress + indicatorsEndpoint
+	res, err := http.Get(endpoint)
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+	jsonRes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	filter.UnmarshalJSON(jsonRes)
+
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.OnRequest(IsMalwareRequestHttp()).DoFunc(GetPiSecPage)
 	proxy.OnRequest(IsMalwareRequestHttps()).HandleConnect(goproxy.AlwaysReject)
 	proxy.Verbose = true
 
-	log.Fatal(http.ListenAndServe(":8080", proxy))
+	log.Fatal(http.ListenAndServe(":8880", proxy))
 }
 
 func GetPiSecPage(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
@@ -35,7 +58,7 @@ func IsMalwareRequestHttp() goproxy.ReqConditionFunc {
 	return func(req *http.Request, ctx *goproxy.ProxyCtx) bool {
 		fmt.Println("checking...")
 		fmt.Println(req.Host)
-		return strings.Split(req.Host, ":")[0] == "news.ycombinator.com" //todo check bloom filter
+		return filter.TestString(strings.Split(req.Host, ":")[0])
 	}
 }
 
@@ -43,7 +66,7 @@ func IsMalwareRequestHttps() goproxy.ReqConditionFunc {
 	return func(req *http.Request, ctx *goproxy.ProxyCtx) bool {
 		fmt.Println("checking...")
 		fmt.Println(req.Host)
-		return strings.Split(req.Host, ":")[0] == "news.ycombinator.com" //todo check bloom filter
+		return filter.TestString(strings.Split(req.Host, ":")[0])
 	}
 }
 
