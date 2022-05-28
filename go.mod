c07d40d6f93278f6e767960dc7135420c5a13706
Alessio Pascucci <feeder81@gmail.com>
2022-05-28 23:34:11 +0200

    Implement PiSec REDIS cache + tests
    A new component, with package name "cache", is implemented using REDIS. The organization is made using KEY naming and : character as separator.
    The main schema is PISEC: and then we have 3 keyset
    
    - ALLOW: urls that are allowed by the user
    - DENY: this is a real cache of the BRAIN repository, containing blocked URLs
    - FALSE_POSITIVE: this keyset contains URLs that have been marked positive from the Bloom Filter, but it's not really present in the BRAIN repository.
    
    This cache will be then used to implement the validation protocol for phishing URLs

--- go.mod
+++ go.mod
@@ -9,10 +9,15 @@ require (
 
 require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
 
 require (
 	github.com/bits-and-blooms/bitset v1.2.0 // indirect
+	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/go-redis/redis/v8 v8.11.5
+	github.com/stretchr/testify v1.7.1
 )
---
+++ redis/redis_test.go
@@ -0,0 +1,128 @@
+package cache
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRedisClient(t *testing.T) {
+	//given
+	repo := NewRedisClient()
+
+	//when
+	pong, err := repo.client.Ping().Result()
+
+	//then
+	require.Nil(t, err)
+	require.Equal(t, pong, "PONG")
+}
+
+func TestRedisInit(t *testing.T) {
+
+	//given
+	repo := NewRedisClient()
+	repo.InitRepository()
+
+	//when
+	err := repo.AddAllow("ALLOW_EXAMPLE")
+	require.Nil(t, err)
+
+	//then
+	val, err := repo.GetRepoSize()
+	require.Nil(t, err)
+	require.Equal(t, val, 1)
+
+	err = repo.InitRepository()
+	require.Nil(t, err)
+
+	val, err = repo.GetRepoSize()
+	require.Nil(t, err)
+	require.Equal(t, val, 0)
+
+}
+
+func TestRedisInsertAllow(t *testing.T) {
+	//given
+	repo := NewRedisClient()
+	repo.InitRepository()
+
+	//when
+	err := repo.AddAllow("ALLOW_EXAMPLE")
+	require.Nil(t, err)
+
+	res, err := repo.isAllow("ALLOW_EXAMPLE")
+	require.Nil(t, err)
+
+	//then
+	require.True(t, res)
+}
+
+func TestRedisInsertDeny(t *testing.T) {
+	//given
+	repo := NewRedisClient()
+	repo.InitRepository()
+
+	//when
+	err := repo.AddDeny("DENY_EXAMPLE")
+	require.Nil(t, err)
+
+	res, err := repo.IsDeny("DENY_EXAMPLE")
+	//then
+	require.Nil(t, err)
+	require.True(t, res)
+}
+
+func TestRedisInsertFalsePositive(t *testing.T) {
+	//given
+	repo := NewRedisClient()
+	repo.InitRepository()
+
+	//when
+	err := repo.AddFalsePositive("FALSE_POSITIVE_EXAMPLE")
+	require.Nil(t, err)
+
+	res, err := repo.IsFalsePositive("FALSE_POSITIVE_EXAMPLE")
+	//then
+	require.Nil(t, err)
+	require.True(t, res)
+}
+
+func TestRedisMissingDeny(t *testing.T) {
+	//given
+	repo := NewRedisClient()
+	repo.InitRepository()
+
+	//when
+	res, err := repo.IsDeny("DENY_EXAMPLE")
+
+	//then
+	require.Nil(t, err)
+	require.False(t, res)
+}
+
+func TestRedisMissingAllow(t *testing.T) {
+	//given
+	repo := NewRedisClient()
+	repo.InitRepository()
+
+	//when
+	res, err := repo.IsDeny("ALLOW_EXAMPLE")
+
+	//then
+	require.Nil(t, err) //Object not found is a special error case
+	require.False(t, res)
+}
+
+func TestRedisMissingFalsePositive(t *testing.T) {
+	//given
+	repo := NewRedisClient()
+	repo.InitRepository()
+
+	//when
+	res, err := repo.IsFalsePositive("FALSE_POSITIVE_EXAMPLE")
+
+	//then
+	require.Nil(t, err) //Object not found is a special error case
+	require.False(t, res)
+}
