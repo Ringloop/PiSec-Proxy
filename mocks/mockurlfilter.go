d32028ac156617b2bf285d608759a0b1579b5cf2
Alessio Pascucci <feeder81@gmail.com>
2022-06-28 23:26:26 +0200

    clean code

--- mocks/mockurlfilter.go
+++ mocks/mockurlfilter.go
@@ -1,22 +1,10 @@
 package mocks
 
-import (
-	"github.com/Ringloop/pisec/brainclient"
-	"github.com/Ringloop/pisec/cache"
-)
-
 type MockUrlFilter struct {
 	CheckInFilterFunc func(url string) bool
-	Client            brainclient.BrainClient
-	Repo              cache.RepoClient
 }
-
-var (
-	// CheckInFilterFunc fetches the mock client's `CheckUrlInBloom` func
-	CheckInFilterFunc func(url string) bool
-)
 
 // CheckUrlInBloom is the mock client's `CheckUrlInBloom` func
 func (m *MockUrlFilter) CheckUrlInBloom(url string) bool {
-	return CheckInFilterFunc(url)
+	return m.CheckInFilterFunc(url)
 }
